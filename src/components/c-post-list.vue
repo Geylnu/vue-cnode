b1d30c425dd73dc3835d4b02661af2e20c4a9102
geylnu <yanglei1997630@gmail.com>
2019-06-18 22:03:15 +0800

    完成滚动加载实现

--- src/components/c-post-list.vue
+++ src/components/c-post-list.vue
@@ -13,9 +13,11 @@
         <div class="topics">
           <c-topicCell v-for="topic of postList" :key="topic.id" :topic="topic"></c-topicCell>
         </div>
-        <div class="pagination"></div>
+        <div class="loadMore" v-show="!loadMore">已经看完{{page}}页了，再次下滑加载更多</div>
+        <div class="loadMore" v-show="loadMore">加载中。。。</div>
       </main>
     </div>
+    <div class="test" v-if="readyLoadMore"></div>
   </div>
 </template>
 
@@ -34,8 +36,12 @@ export default {
         { key: "ask", text: "问答" },
         { key: "job", text: "招聘" }
       ],
+      page: 1,
+      limit: 40,
       postList: [],
-      title: "CNode：Node.js专业中文社区"
+      title: "CNode：Node.js专业中文社区",
+      readyLoadMore: false,
+      loadMore: false
     };
   },
   components: {
@@ -46,11 +52,17 @@ export default {
       let {
         data: { data, success }
       } = await this.$http.get(api.topics, {
-        params: { tab: this.$route.params.tab }
+        params: {
+          tab: this.$route.params.tab,
+          page: this.page,
+          limit: this.limit
+        }
       });
       if (success) {
-        this.postList = data;
+        this.postList =
+          this.postList.length !== 0 ? this.postList.concat(data) : data;
       }
+      return success
     }
   },
   watch: {
@@ -61,15 +73,46 @@ export default {
   created() {
     this.getData();
   },
-  filters: {
-    topicUrl(id) {
-      return api.topic + id;
+  mounted() {
+    this.scrollLoad = (e) => {
+      let sTop = document.documentElement.scrollTop
+      let vh = window.innerHeight || document.documentElement.clientHeight;
+      let dh = document.body.clientHeight
+      if ( dh - sTop -vh === 0) {
+        if (!this.readyLoadMore){
+          setTimeout(() => {
+            this.readyLoadMore = true;
+          }, 400);
+        }
+
+        if (this.readyLoadMore === true && !this.loadMore) {
+          this.page += 1;
+          this.loadMore = true;
+          this.getData().then((success) => {
+            if (success) {
+              this.loadMore = false;
+              this.readyLoadMore = false
+            } else {
+              this.loadMoreSuccess = false;
+            }
+          });
+        }
+      }
     }
+    document.addEventListener("scroll",this.scrollLoad);
+  },
+  destroyed(){
+    document.removeEventListener('scroll',this.scrollLoad)
   }
 };
 </script>
 
 <style scoped>
+.test {
+  height: 1em;
+  background-color: transparent;
+  visibility: hidden;
+}
 .panel.topicList {
   font-size: 15px;
   line-height: 2em;
@@ -89,6 +132,10 @@ export default {
   color: #fff;
   padding: 3px 5px;
   border-radius: 3px;
+}
+
+.loadMore {
+  text-align: center;
 }
 </style>
 
