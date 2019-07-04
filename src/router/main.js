b4e2d4adb5948d2ff4b19fe74dec82cdbf23b52e
geylnu <yanglei1997630@gmail.com>
2019-07-04 11:02:07 +0800

    添加滚动行为

--- src/router/main.js
+++ src/router/main.js
@@ -43,7 +43,14 @@ const router = new VueRouter({
         main: cUserinfo,
       }
     }
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
 
 export default router
